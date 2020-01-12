9b42e1f57e73e95b4f9a22cb099ec2f080671c9f
Kevin Cheng <kevin83321@gmail.com>
2020-01-12 22:35:36 +0800

    new practice
    OOP
    Constuctor
    Reference type

--- OOP_Practice1/OOP_Practice1/Form1.cs
+++ OOP_Practice1/OOP_Practice1/Form1.cs
@@ -19,12 +19,43 @@ namespace OOP_Practice1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Student s = new Student();
-            s.Name = "Kevin";
-            s.Grade = 7;
-            s.StudentID = 55688;
+            //Student s = new Student();
+            //s.Name = "Kevin";
+            //s.Grade = 7;
+            //s.StudentID = 55688;
 
-            MessageBox.Show(s.Say());
+            ////MessageBox.Show(s.Say());
+            ////s.Upgrade();
+            ////MessageBox.Show(s.Say());
+            ////MessageBox.Show(s.Talk(9, "cc"));
+
+            //Student s2 = new Student();
+            //s2.Name = "kiki";
+            //s2.Grade = 3;
+            //MessageBox.Show(s.Talk(s2));
+
+            //// value type
+            ////int a = 10;
+            ////int b = a;
+            ////b = 20;
+            ////MessageBox.Show("a = " + a + ", b = " + b);
+
+            //// reference type
+            ////Student s1 = new Student();
+            ////s1.Name = "kkk";
+            ////s1.Grade = 5;
+
+            ////Student s3 = s1;
+            ////s3.Name = "iii";
+            ////s3.Grade = 7;
+            ////MessageBox.Show(s1.Say());
+
+            // Constuctor
+
+            Student s1 = new Student(10201, "aaa");
+
+            Student s2 = new Student(10202, "bbb", 2);
+
         }
     }
 }
--- OOP_Practice1/OOP_Practice1/Student.cs
+++ OOP_Practice1/OOP_Practice1/Student.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace OOP_Practice1
 {
@@ -17,7 +18,37 @@ namespace OOP_Practice1
         // public OutputType MethodName(Input Type and Name)
         public string Say()
         {
-            return "我叫 " + Name + " ，我是 " + Grade + " 年級的學生";
+            return "我叫 " + Name + " ，我是 " + Grade + " 年級的學生，我的學號是 " + StudentID;
+        }
+        public void Upgrade()
+        {
+            Grade++;
+        }
+        //public string Talk(int sgrade, string sname)
+        //{
+        //    return Grade + " 年級的 " + Name + " 對 " + sgrade + " 年級的 " + sname + " 說你好!";
+        //}
+
+        public string Talk(Student s2)
+        {
+            return Grade + " 年級的 " + Name + " 對 " + s2.Grade + " 年級的 " + s2.Name + " 說你好!";
+        }
+
+        public Student(int studentID, string name) // Constructor 建構子，用於初始化
+        {
+            Grade = 1;
+            Name = name;
+            StudentID = studentID;
+            
+            MessageBox.Show(Say());
+        }
+        public Student(int studentID, string name, int grade) // Constructor 建構子，用於初始化
+        {
+            Grade = grade;
+            Name = name;
+            StudentID = studentID;
+
+            MessageBox.Show(Say());
         }
     }
 }
